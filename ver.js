fc4cdf85d785321e74be6db86c8f5dd01d97a8e3
张浩 <zhanghao@qiyi.com>
2016-08-24 13:09:22 +0800

    delete ver.js

--- ver.js
+++
@@ -1,7 +0,0 @@
-(function() {
-	var node = document.createElement('script');
-	// 此处维护版本号
-	var version = 'build/a0c746c72906857d8528';
-	node.setAttribute('src', './'+ version +'/app.bundle.js');
-	document.getElementById('body').appendChild(node);
-})();
